d5467bd280b2a1e9c70585ec3e67a5a5c7f7265f
Aleksey Lavrukhin <alexudracul@gmail.com>
2020-07-24 11:49:13 +0300

    Initial Commit

---
+++ LocationNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationNotes
+//
+//  Created by Aleksey Lavrukhin on 24.07.2020.
+//  Copyright © 2020 Aleksey Lavrukhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
